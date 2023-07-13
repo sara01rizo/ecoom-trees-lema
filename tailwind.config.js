57bfad726f9fac2082095788a4f7c94dcbcebe06
sara pulido <sara01rizo@gmail.com>
2023-07-13 12:26:00 +0200

    Mod: tailwind config - Assets up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-bluegreen":
+          "linear-gradient(25deg, #2d4cdf, #6080ba, #64b391, #3ee75d)",
+        "mobile-home": "url('./assets/pexels-pixabay-235615.jpg')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
     screens: {
       xs: "480px",
       sm: "768px",
@@ -11,3 +38,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+// Here you find all the themes colors
