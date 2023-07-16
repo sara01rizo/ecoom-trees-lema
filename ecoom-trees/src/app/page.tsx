6675904928e647c7c1f4978c011317d9fce4ad6e
sara pulido <sara01rizo@gmail.com>
2023-07-17 00:48:22 +0200

    Create: component Navbar Action button shared and so important types

--- ecoom-trees/src/app/page.tsx
+++ ecoom-trees/src/app/page.tsx
@@ -1,9 +1,34 @@
-export default function Home() {
+import Navbar from "@/app/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/app/shared/types";
+
+function Home() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <main>
-      <div>
-        <p>Hello Hallo Hola</p>
-      </div>
-    </main>
+    <div className="home bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
   );
 }
+
+export default Home;
